266637b161292a414e7fd79cbfae13a1d7403562
anidaniele <daniele.anilionyte@gmail.com>
2024-02-22 19:07:21 +0200

    Task5: created class with generic types, one of which accepts only Numbers

---
+++ src/Task5/Task5.java
@@ -0,0 +1,11 @@
+package Task5;
+
+public class Task5 {
+    public static void main(String[] args) {
+        SuperPair superPair1 = new SuperPair<>("A", "B", 1);
+        SuperPair superPair2 = new SuperPair<>("A", "B", 10d);
+        SuperPair superPair3 = new SuperPair<>(1, 2.5f, 10d);
+
+
+    }
+}
